b04f04e703c8e0f860ba933bb7c8e688b8df9af3
rodgnathaniel <rodgnathaniel@gmail.com>
2019-05-13 21:34:22 -0700

    learning beautiful soup

--- python_solutions/web_crawler.py
+++ python_solutions/web_crawler.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+def spider(mp):
+    page = 1
+    while page < mp:
+        url = 'http://nathantrodgers.pythonanywhere.com/'
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        
