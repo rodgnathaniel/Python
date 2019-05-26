772de093c5fdde50817e3938912b4b7255bda550
rodgnathaniel <rodgnathaniel@gmail.com>
2019-05-25 23:58:14 -0700

    \

--- python_solutions/web_crawler.py
+++ python_solutions/web_crawler.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 def spider(mp):
     page = 1
     while page < mp:
-        url = 'http://nathantrodgers.pythonanywhere.com/'
-        source_code = requests.get(url)
+        scrape_url = 'http://nathantrodgers.pythonanywhere.com/'
+        source_code = requests.get(scrape_url)
         source_text = source_code.text
         extracted_txt = BeautifulSoup(source_text)
             for link in extracted_txt.findAll('a', {'class': 'item_name'}):
